2de112ae63b9a799a00cfd6ca03f0f9a116c82b5
Gregory Utkin <grishautkin@gmail.com>
2021-09-25 10:45:46 -0400

    fixed up

--- top100_billboard.R
+++ top100_billboard.R
@@ -1,23 +1,32 @@
+# load packages 
 library(tidyverse)
 library(lubridate)
 library(tidymodels)
+library(lubridate)
 theme_set(theme_minimal())
 
-
+# load data
 billboard <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-09-14/billboard.csv')
 audio <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-09-14/audio_features.csv')
 
 glimpse(billboard)
 glimpse(audio)
 
-dfm <- billboard %>%
-  left_join(audio, by = c("song_id", "performer", "song")) %>%
-  mutate(week_id = lubridate::mdy(week_id),
-         year = lubridate::year(week_id))
+# group the dataset by max weeks of chart
+bb_group <- billboard %>%
+  group_by(song_id) %>%
+  summarise(max_weeks = max(weeks_on_chart), .groups = "drop")
+
+
+colSums(is.na(audio))
+# join into one dataset
+dfm <- audio %>%
+  filter(!is.na(spotify_track_popularity)) %>%
+  inner_join(bb_group)
 
 
 
-dfm %>%
+billboard %>%
   filter(week_position == 1) %>%
   count(song, performer, sort = TRUE) %>%
   top_n(15) %>%
@@ -33,7 +42,10 @@ billboard %>%
   summarise(weeks_on_chart = max(weeks_on_chart), .groups = "drop") %>%
   arrange(desc(weeks_on_chart))
 
-dfm %>%
+billboard %>%
+  left_join(audio, by = "song_id") %>%
+  mutate(week_id = lubridate::mdy(week_id),
+         year = lubridate::year(week_id)) %>%
   group_by(year) %>%
   summarise(tempo = median(tempo, na.rm = TRUE)) %>%
   ggplot(aes(x = year, y = tempo)) +
@@ -44,8 +56,7 @@ dfm %>%
 
 # working on it REGEX 
 df_tidy <- dfm %>%
-  filter(year > 2000) %>%
-  select(performer, week_id, spotify_genre, danceability:tempo,-mode) %>%
+  select(performer, spotify_genre, danceability:tempo,-mode) %>%
   mutate(
     genre = str_remove_all(spotify_genre, "\\[*'|'*\\]"),
     # got to clean genre
@@ -53,16 +64,18 @@ df_tidy <- dfm %>%
   ) %>% 
   select(-spotify_genre) %>% 
   separate(genre, c("one", "two", "three", "four"), sep = ",", fill = "left", extra = "merge") %>%
-  mutate(one = replace_na(one, "none"),
-         two = replace_na(two, "none"),
-         three = replace_na(three, "none"),
-         four = replace_na(four, "none")) %>%
-  count(one, two, three, four, sort = TRUE)
+  mutate(one = replace_na(one, "unknown"),
+         two = replace_na(two, "unknown"),
+         three = replace_na(three, "unknown"),
+         four = replace_na(four, "unknown")) %>%
+  select(-c(one, two, four))
+
+
 
 rec <- recipe(~., data = df_tidy) %>%
-  update_role(spotify_genre, performer, week_id, new_role = "id") %>%
+  update_role(three, performer, new_role = "id") %>%
   step_normalize(all_predictors()) %>%
-  step_impute_median(all_predictors()) %>%
+  step_impute_median(all_numeric_predictors()) %>%
   step_pca(all_numeric_predictors(), num_comp = 4)
 
 
@@ -99,6 +112,7 @@ pca_tidied %>%
     fill = "Positive?"
   )
 
+# get it into a tibble for future work.
 juice(pca_prep)
 
 
