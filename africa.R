3948a4466182c8b7508fccde5215587f869cd345
gregoryut <grishautkin@gmail.com>
2021-11-27 11:15:46 -0500

    week 46 africa population

--- africa.R
+++ africa.R
@@ -4,33 +4,33 @@ library(afrilearndata)
 library(tidyverse)
 library(patchwork)
 library(ggsflabel)
-
-africapitals
-afriairports
+library(scico)
 
 
 africa_pop <- afripop2020 %>%
   as.data.frame(xy = TRUE) %>%
-  drop_na() %>%
-  mutate(pop = ifelse(ppp_2020_1km_Aggregated < 500,
-                      ppp_2020_1km_Aggregated, 600))
+  drop_na(ppp_2020_1km_Aggregated) %>%
+  rename(pop = ppp_2020_1km_Aggregated)
 
 
 
 pop2020 <- ggplot() +
   geom_tile(data = africa_pop, aes(x, y, fill = pop)) +
-  scale_fill_gradient2() +
+  scale_fill_scico(direction = -1, 
+                   trans = "pseudo_log",
+                   palette = "lajolla", 
+                   breaks = c(0, 100, 1000, 10000, 20000)) +
   theme_minimal() +
   labs(fill = "Population") +
   geom_sf(
     data = africapitals,
-    alpha = 0.2,
+    alpha = 0.3,
     color = "darkorange",
-    aes(size = pop)
+    aes(size = (pop))
   ) +
-  geom_sf_label_repel(data = africapitals, aes(label = capitalname), 
-                      color = "grey10", family = "prata", size = 2.5, nudge_x = -0.5,
-                      segment.color = "white", segment.size = 0.5) + 
+  #geom_sf_label_repel(data = africapitals, aes(label = capitalname), 
+  #                    color = "grey10", family = "prata", size = 2.5, nudge_x = -1.5,
+  #                    segment.color = "transparent", segment.size = 1) + 
   labs(x = NULL,
        y = NULL,
        size = "City Population",
@@ -42,20 +42,20 @@ pop2020 <- ggplot() +
   )
 
 
+pop2020
 
-ggplot(africa_pop, aes(x, y, fill = pop)) +
-  geom_raster() +
-  theme_minimal() +
-  theme(panel.grid = element_blank())
+
 
 pop2000 <- afripop2000 %>%
-  as.data.frame(xy = TRUE) %>%
-  drop_na() %>%
-  mutate(pop = ifelse(ppp_2000_1km_Aggregated < 500,
-                      ppp_2000_1km_Aggregated, 600)) %>%
+  as.data.frame(xy = TRUE) %>% 
+  drop_na(ppp_2000_1km_Aggregated) %>%
+  rename(pop = ppp_2000_1km_Aggregated) %>%
   ggplot() +
   geom_tile(aes(x, y, fill = pop)) +
-  scale_fill_gradient2() +
+  scale_fill_scico(direction = -1, 
+                   trans = "pseudo_log",
+                   palette = "lajolla", 
+                   breaks = c(0, 100, 1000, 10000, 20000)) +
   theme_minimal() +
   labs(fill = "Population") +
   geom_sf(
@@ -64,9 +64,9 @@ pop2000 <- afripop2000 %>%
     color = "darkorange",
     aes(size = pop)
   ) +
-  geom_sf_label_repel(data = africapitals, aes(label = capitalname), 
-                      color = "grey10", family = "prata", size = 2.5, nudge_x = -0.5,
-                      segment.color = "white", segment.size = 0.5) + 
+  #geom_sf_label_repel(data = africapitals, aes(label = capitalname), 
+  #                    color = "grey10", family = "prata", size = 2.5, nudge_x = -0.5,
+  #                    segment.color = "white", segment.size = 0.5) + 
   labs(x = NULL,
        y = NULL,
        size = "City Population",
