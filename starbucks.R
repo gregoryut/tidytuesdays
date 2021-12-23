00f43e658e0d109ae9775a84f1f8c1f94b93fe31
gregoryut <grishautkin@gmail.com>
2021-12-23 09:45:22 -0500

    starbucks week 52

--- starbucks.R
+++ starbucks.R
@@ -101,17 +101,63 @@ starbucks %>%
   facet_wrap(~name)
 
 
+# Xgboost to predict sugar grams
 
+splits <- initial_split(starbucks, prop = .7)
+train <- training(splits)
+test <- testing(splits)
 
+rec <- recipe(sugar_g ~ ., data = train) %>%
+  step_dummy(all_nominal_predictors(), one_hot = TRUE) %>%
+  step_other(all_nominal_predictors(), threshold = .3)
 
+rec %>%
+  prep(train) %>%
+  juice()
 
+xgb_spec <- boost_tree(mode = 'regression',
+                       engine = "xgboost", 
+                       mtry = tune(), 
+                       trees = tune(), 
+                       learn_rate = tune(),
+                       min_n = tune())
 
+xg_wf <- workflow(rec, xgb_spec)
 
+folds <- vfold_cv(train)
 
+xgb_tune <- xg_wf %>%
+  tune_grid(folds,
+            metrics = metric_set(rmse, mae),
+            control = control_grid(verbose = TRUE))
 
+autoplot(xgb_tune)
 
+xgb_best <- xg_wf %>%
+  finalize_workflow(select_best(xgb_tune))
 
+xgb_best_fit <- xgb_best %>%
+  fit(train)
 
+xgb_best_fit %>%
+  augment(test) %>% 
+  ggplot(aes(sugar_g, .pred)) +
+  geom_point() +
+  labs(x = "sugar_g actual",
+       y = "sugar_g predicted") +
+  geom_smooth(method = 'lm', alpha = 0.4) +
+  theme_minimal()
+
+xgb_best_fit %>%
+  extract_fit_engine() %>%
+  vip::vip(geom = 'point', num_features = 10) +
+  geom_point(aes(color = "red")) +
+  theme_minimal() +
+  labs(title = "Variable Importance XG-Boost") +
+  theme(panel.grid = element_blank(),
+        legend.position = 'none',
+        plot.title = element_text(hjust = 0.5, face = 'bold', size = 17))
+  
 
 
 
