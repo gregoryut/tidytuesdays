af0ed4ff4558e7e96c16ae5d6fdc518c95e4b08d
gregoryut <grishautkin@gmail.com>
2022-01-02 23:36:21 -0500

    week 49 toronto shelters

--- 2020/toronto_shelters.R
+++ 2020/toronto_shelters.R
@@ -5,7 +5,7 @@ shelters <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/t
 
 glimpse(shelters)
 
-  max(shelters$occupancy_date)
+max(shelters$occupancy_date)
 min(shelters$occupancy_date)
 
 shelters %>% count(facility_name, sort = TRUE)
@@ -21,7 +21,7 @@ shelters %>%
   summarise(occ = mean(rate, na.rm = TRUE)) %>%
   ggplot_calendar_heatmap(cDateColumnName = 'occupancy_date',
                           cValueColumnName = 'occ',
-                          dayBorderSize = 1, 
+                          dayBorderSize = .5, 
                           monthBorderSize = 1) +
   scale_fill_viridis_c(labels = scales::percent) +
   facet_wrap(~year, ncol = 1) +
